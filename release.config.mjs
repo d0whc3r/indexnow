4618ea6bc10f1f2125b8caf0bc316352bbc4a859
d0whc3r <1378986+d0whc3r@users.noreply.github.com>
2024-08-09 16:53:02 +0200

    fix: update content

--- release.config.mjs
+++ release.config.mjs
@@ -9,22 +9,17 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
+    '@semantic-release/npm',
     [
       '@semantic-release/github',
       {
         assets: 'dist/*',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
       },
     ],
     [
